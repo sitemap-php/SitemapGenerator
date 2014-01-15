d80e9509cb2ea53c5e2e432a17a41474ee24a47a
Kévin Gomez <geek63@gmail.com>
2014-01-15 15:56:48 +0000

    Fixed tests

--- tests/SitemapGenerator/Tests/Provider/PropelProviderTest.php
+++ tests/SitemapGenerator/Tests/Provider/PropelProviderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SitemapGenerator\Tests\Entity;
+namespace SitemapGenerator\Tests\Provider;
 
 use \Mockery;
 
@@ -11,7 +11,7 @@ use SitemapGenerator\Tests\Fixtures\News;
 /**
  * @runTestsInSeparateProcesses
  */
-class PropelProviderTest extends \PHPUnit_Framework_TestCase
+class PropelProviderTest extends AbstractProviderTest
 {
     /**
      * @dataProvider newsDataProvider
@@ -37,30 +37,9 @@ class PropelProviderTest extends \PHPUnit_Framework_TestCase
         ));
     }
 
-    protected function getRouter(array $results)
-    {
-        $router = $this->getMockBuilder('\Symfony\Component\Routing\RouterInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $valueMap = array();
-        foreach ($results as $news) {
-            $valueMap[] = array('show_news', array(
-                'id' => $news->slug
-            ), false, '/news/'.$news->slug);
-        }
-
-        $router
-            ->expects($this->any())
-            ->method('generate')
-            ->will($this->returnValueMap($valueMap));
-
-        return $router;
-    }
-
     protected function getPropelQuery($model, $results)
     {
-        $mock = Mockery::mock('overload:\PropelQuery');
+        $mock = Mockery::mock('overload:PropelQuery');
 
         $mock
             ->shouldReceive('from')
@@ -79,7 +58,7 @@ class PropelProviderTest extends \PHPUnit_Framework_TestCase
             ->once()
             ->andReturn($results);
 
-        return $this->propelMock = $mock;
+        return $mock;
     }
 
     protected function getNewsProvider(array $results)
@@ -88,47 +67,5 @@ class PropelProviderTest extends \PHPUnit_Framework_TestCase
             'model' => 'SitemapGenerator\Tests\Fixtures\News',
             'loc'   => array('route' => 'show_news', 'params' => array('id' => 'slug')),
         ));
-    }
-
-    protected function getSitemap(array $urls)
-    {
-        $sitemap = $this->getMockBuilder('\SitemapGenerator\Sitemap\Sitemap')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        if (empty($urls)) {
-            $sitemap
-                ->expects($this->never())
-                ->method('add');
-        } else {
-            foreach ($urls as $at => $url) {
-                $sitemap
-                    ->expects($this->at($at))
-                    ->method('add')
-                    ->with($this->equalTo($url));
-            }
-        }
-
-        return $sitemap;
-    }
-
-    public function newsDataProvider()
-    {
-        $first = new News();
-        $first->slug = 'first';
-
-        $second = new News();
-        $second->slug = 'second';
-
-        $urlFirst = new Url();
-        $urlFirst->setLoc('/news/first');
-
-        $urlSecond = new Url();
-        $urlSecond->setLoc('/news/second');
-
-        return array(
-            array(array(), array()),
-            array(array($first, $second), array($urlFirst, $urlSecond)),
-        );
     }
 }
