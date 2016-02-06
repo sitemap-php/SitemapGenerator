4a6679e95fc10ce47ceaf04440c5770be4bd3da5
Kévin Gomez <contact@kevingomez.fr>
2016-02-06 15:55:30 +0100

    Remove Propel and Doctrine providers

--- tests/Provider/RouteTest.php
+++ tests/Provider/RouteTest.php
@@ -2,9 +2,11 @@
 
 namespace SitemapGenerator\Tests\Provider;
 
+use SitemapGenerator\Entity\Url;
 use SitemapGenerator\Provider\Route as RouteProvider;
+use SitemapGenerator\Tests\Fixtures\News;
 
-class RouteProviderTest extends AbstractProviderTest
+class RouteProviderTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider newsDataProvider
@@ -29,5 +31,40 @@ class RouteProviderTest extends AbstractProviderTest
         return new RouteProvider($this->getRouter($results), [
             'routes' => $routes,
         ]);
+    }
+
+    protected function getRouter(array $results)
+    {
+        $router = $this->getMock('\SitemapGenerator\UrlGenerator');
+
+        $valueMap = array_map(function(News $news) {
+            return [
+                'show_news', ['id' => $news->slug], '/news/' . $news->slug
+            ];
+        }, $results);
+
+        $router
+            ->expects($this->any())
+            ->method('generate')
+            ->will($this->returnValueMap($valueMap));
+
+        return $router;
+    }
+
+    public function newsDataProvider()
+    {
+        $first = new News();
+        $first->slug = 'first';
+
+        $second = new News();
+        $second->slug = 'second';
+
+        $urlFirst = new Url('/news/first');
+        $urlSecond = new Url('/news/second');
+
+        return [
+            [[], []],
+            [[$first, $second], [$urlFirst, $urlSecond]],
+        ];
     }
 }
